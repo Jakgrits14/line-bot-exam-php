adcdf6f24ee584da8cade582d7abc0b6a0773d26
Jakgrits14 <37709056+Jakgrits14@users.noreply.github.com>
2018-03-29 13:10:24 +0700

    Update index.php

--- index.php
+++ index.php
@@ -1,11 +1,63 @@
 <?php
-require "vendor/autoload.php";
-$access_token = '5AlCUC043hOJqKAofMoPLcdvATJH2BLdlwJ3/lBlKOM2/7FpjTTA/NNFjTSrGCnHwOzB7jmQhPD5upGPH9uunYoTRCvsAMoID62PkBhRAppFEUQklhq48wJOngZTep4kungvsl/n0F9CCP1hOEihBAdB04t89/1O/w1cDnyilFU=';
-$channelSecret = '86fdda7e68a0484a6d79a81640dd1871';
-$idPush = 'Ue8c4c0ea11b3c76c1f787be5e384b496'
-$httpClient = new \LINE\LINEBot\HTTPClient\CurlHTTPClient($access_token);
-$bot = new \LINE\LINEBot($httpClient, ['channelSecret' => $channelSecret]);
-$textMessageBuilder = new \LINE\LINEBot\MessageBuilder\TextMessageBuilder('hello world');
-$response = $bot->pushMessage('Ue8c4c0ea11b3c76c1f787be5e384b496', $textMessageBuilder);
 
-echo $response->getHTTPStatus() . ' ' . $response->getRawBody();
+$API_URL = 'https://api.line.me/v2/bot/message/reply';
+$access_token = '5AlCUC043hOJqKAofMoPLcdvATJH2BLdlwJ3/lBlKOM2/7FpjTTA/NNFjTSrGCnHwOzB7jmQhPD5upGPH9uunYoTRCvsAMoID62PkBhRAppFEUQklhq48wJOngZTep4kungvsl/n0F9CCP1hOEihBAdB04t89/1O/w1cDnyilFU='; // Access Token ค่าที่เราสร้างขึ้น
+$POST_HEADER = array('Content-Type: application/json', 'Authorization: Bearer ' . $ACCESS_TOKEN);
+
+$request = file_get_contents('php://input');   // Get request content
+$request_array = json_decode($request, true);   // Decode JSON to Array
+
+if ( sizeof($request_array['events']) > 0 )
+{
+
+ foreach ($request_array['events'] as $event)
+ {
+  $reply_message = '';
+  $reply_token = $event['replyToken'];
+
+  if ( $event['type'] == 'message' ) 
+  {
+   if( $event['message']['type'] == 'text' )
+   {
+    $text = $event['message']['text'];
+    $reply_message = 'ระบบได้รับข้อความ ('.$text.') ของคุณแล้ว';
+   }
+   else
+    $reply_message = 'ระบบได้รับ '.ucfirst($event['message']['type']).' ของคุณแล้ว';
+  
+  }
+  else
+   $reply_message = 'ระบบได้รับ Event '.ucfirst($event['type']).' ของคุณแล้ว';
+ 
+  if( strlen($reply_message) > 0 )
+  {
+   //$reply_message = iconv("tis-620","utf-8",$reply_message);
+   $data = [
+    'replyToken' => $reply_token,
+    'messages' => [['type' => 'text', 'text' => $reply_message]]
+   ];
+   $post_body = json_encode($data, JSON_UNESCAPED_UNICODE);
+
+   $send_result = send_reply_message($API_URL, $POST_HEADER, $post_body);
+   echo "Result: ".$send_result."\r\n";
+  }
+ }
+}
+
+echo "OK";
+
+function send_reply_message($url, $post_header, $post_body)
+{
+ $ch = curl_init($url);
+ curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "POST");
+ curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+ curl_setopt($ch, CURLOPT_HTTPHEADER, $post_header);
+ curl_setopt($ch, CURLOPT_POSTFIELDS, $post_body);
+ curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
+ $result = curl_exec($ch);
+ curl_close($ch);
+
+ return $result;
+}
+
+?>
