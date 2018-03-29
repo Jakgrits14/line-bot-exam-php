1bfa860675719ba095a116269f8299f8a740982a
Jakgrits14 <37709056+Jakgrits14@users.noreply.github.com>
2018-03-29 12:54:39 +0700

    Update webhooks.php

--- webhooks.php
+++ webhooks.php
@@ -3,7 +3,7 @@
 require "vendor/autoload.php";
 require_once('vendor/linecorp/line-bot-sdk/line-bot-sdk-tiny/LINEBotTiny.php');
 
-$access_token = '3ALKAbKFoGuJyJnoDdn0HeyfbxLFtEXBKiC0lFeoNl/XbL4WhoCZzefp2n7UDuXaCWfErIDro07BnZNggJmXJChXTIlMPo8LRJ+n1LEgbRUaKehDkiCr5p5CakHrPX+gauOGX/R5bB2e5yi7xjnHDAdB04t89/1O/w1cDnyilFU=';
+$access_token = '5AlCUC043hOJqKAofMoPLcdvATJH2BLdlwJ3/lBlKOM2/7FpjTTA/NNFjTSrGCnHwOzB7jmQhPD5upGPH9uunYoTRCvsAMoID62PkBhRAppFEUQklhq48wJOngZTep4kungvsl/n0F9CCP1hOEihBAdB04t89/1O/w1cDnyilFU=';
 
 // Get POST body content
 $content = file_get_contents('php://input');
